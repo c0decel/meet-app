2ac7058d4f305f21083e48ed3667534695530def
c0decel <seismicm0ke@proton.me>
2023-09-17 03:53:14 -0700

    4.4 redo

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,48 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Event from '../components/Event';
+import { getEvents } from '../api';
+
+describe('<Event /> component', () => {
+    let EventComponent;
+    let allEvents;
+
+    beforeAll(async () => {
+        allEvents = await getEvents();
+      });
+
+    beforeEach(() => {
+        EventComponent = render(<Event event={allEvents[0]} />);
+      });
+
+    test("renders event name", () => {
+        expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
+      });
+
+    test('renders event location', () => {
+      expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
+    });
+
+    test('has a showdetails button', () => {
+      const button = EventComponent.queryByRole('button');
+      expect(button).toBeInTheDocument();
+    });
+
+    test('by default, events are hidden', () => {
+      const eventDetails = EventComponent.queryByText("description");
+     expect(eventDetails).not.toBeInTheDocument();
+   });
+
+    test('shows details upon being clicked', () => {
+    const showDetailsButton = EventComponent.queryByText('Show Details');
+      userEvent.click(showDetailsButton);
+      expect(EventComponent.queryByText(/Have you wondered how you can ask Google/)).toBeInTheDocument();
+    });
+
+    test('hides details upon being clicked', () => {
+      const hideDetailsButton = EventComponent.queryByText('Hide Details');
+        userEvent.click(hideDetailsButton);
+        expect(EventComponent.queryByText(/Have you wondered how you can ask Google/)).toBeInTheDocument();
+      });
+
+});
---
+++ src/components/Event.js
@@ -0,0 +1,23 @@
+import { getEvents } from '../api';
+import { useState } from 'react';
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(true);
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+    return (
+      <li id="event-item">
+        <div className='event'>
+            <div className='summary'>{event.summary}</div>
+            <div className='location'>{event.location}</div>
+           {showDetails && <div className='details'>{event.description}</div> }
+            <button className='detailbutton' onClick={toggleDetails}>
+              {showDetails ? 'Show Details' : 'Hide Details'}
+            </button>
+        </div>
+      </li>
+    );
+  }
+  
+  export default Event;
